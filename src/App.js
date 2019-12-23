7dbc13d15e999df91164bebd8f9a3fae58db790b
Jalaj Rastogi <jalajrastogi4@gmail.com>
2019-12-23 09:17:27 +0530

    added shop page and preview collection items

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ import {Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const Hats = () => (
-  <div>
-    <h1>HATS</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop/hats" component={Hats} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
